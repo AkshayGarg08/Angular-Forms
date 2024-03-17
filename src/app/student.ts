4025cc6c3e02c66b73d46b553d776fcc9ed74943
AkshayGarg08 <akshaygarg911@gmail.com>
2024-03-18 00:29:43 +0530

    reseting form and added some more validations

--- src/app/student.ts
+++ src/app/student.ts
@@ -1,7 +1,15 @@
 export class Student {
 
    
-    constructor(public name:string,public age:number, public email:string) {
+    constructor(
+        public name?:string,
+        public age?:number, 
+        public email?:string,
+        public gender?:string,
+        public country?:String,
+        public accept?:String
+        ) 
+        {
         
     }
 
--- src/app/tdf/tdf.component.ts
+++ src/app/tdf/tdf.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from '../student';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tdf',
@@ -9,15 +10,21 @@ import { Student } from '../student';
 export class TdfComponent {
 
   
-  constructor() {}
+  constructor() {
+    this.std.country="";
+  }
 
-  std = new Student("prem",23,'as@h');
+  std = new Student();
 
 
   save(formData:any){
     // console.log(formData);
     // const std = new Student(formData.name,formData.age,formData.email)
      console.log(this.std)
+     formData.reset();
   }
 
+  resetForm(FormData:NgForm){
+    FormData.resetForm();
+  }
 }
